e6e94b79b26dc479c08e41cc1e83f6d7173e235d
frnmathis71-blip <frnmathis71@gmail.com>
2026-06-02 15:10:02 +0200

    Guard navbar scroll script

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const navbar = document.getElementById("navbar");
 
 function handleNavbarScroll() {
+  if (!navbar) {
+    return;
+  }
+
   if (window.scrollY > 30) {
     navbar.classList.add("scrolled");
   } else {
